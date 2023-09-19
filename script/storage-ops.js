3254666b8e8dee9b7bb15b6f83dde2b0e4ba986d
stevendreed <steven.daniel.reed@gmail.com>
2023-09-18 20:01:55 -0700

    added comments to saveToLS function

--- script/storage-ops.js
+++ script/storage-ops.js
@@ -3,4 +3,14 @@
 
    this file contains functions that save to and retrieve from browser
    local storage.
+
 */
+
+// saveToLS => save to local storage
+// function accepts a value to save and checks if it is a json
+// if it is a json, function stringifies the object first
+// otherwise, function stores the string
+const saveToLS = function(toSave, saveLocation)
+{
+
+}
