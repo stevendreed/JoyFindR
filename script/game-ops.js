7a987a541925f6a30c6d62e4c84ea8feaff6c5d6
stevendreed <steven.daniel.reed@gmail.com>
2023-09-19 19:19:22 -0700

    added basic header comment for game-ops.js

---
+++ script/game-ops.js
@@ -0,0 +1,13 @@
+/* game-ops.js
+   Steven Reed
+   09/19/2023
+
+   this file contains fetch functions which call on the RAWG API
+
+   [+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++]
+   link to the api:
+   https://rawg.io/apidocs
+   [+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++]
+   
+
+*/
