e19a787ec661250aaa4c626d5ab9f18ddce2d28b
stevendreed <steven.daniel.reed@gmail.com>
2023-09-21 20:46:52 -0700

    moved test-related files from game-ops

--- script/app.js
+++ script/app.js
@@ -1,3 +1,65 @@
 // app.js
 
 console.log(`Hello world!`);
+
+
+/*
+   concatQuerPar function = concatenate a query parameter from a series of
+   search terms
+   function combines strings together into a way that is understandable by
+   a server API. Intended use is alongside a fetch function
+*/
+
+// ['game=fallout','tag=shooter']
+const concatQuerPar = function(arrayOfParams)
+{
+    let finalQuery = `?`;
+    // craft a query of concatenated query parameters
+    for (let i = 0; i < arrayOfParams.length; i++)
+    {
+        if (i > 0)
+        {
+            finalQuery += `&`; // add an ampersand if/f more items to loop
+                               // thru AND query isn't a singleton
+        } // end if
+        finalQuery += arrayOfParams[i]; // add the ith param to our query
+    } // end for
+    console.log(finalQuery); // test output for finalQuery
+} // end concatQuerPar
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// TESTING
+// comb document for element to append a button to, for testing
+
+const testBtnEl = document.getElementById(`testing-form`);
+
+testBtnEl.addEventListener(`submit`, function(event)
+{
+    console.log(`submitted!`);
+    event.preventDefault(); // stop page from automatically refreshing
+    
+    // get data entered in input element
+    // let inputData = document.getElementById(`input-field`).formdata
+    let inputData = document.getElementById(`data`).value;
+
+    // console.log(inputData);
+    // pass data as fetch query
+    // rawgFetch(concatQuerPar(inputData), RAWG_URL);
+    console.log(rawgFetch(inputData, RAWG_URL, RAWG_KEY));
+} // end function
+); // end testBtnEl
+
+// find body element
+const bodyEl = document.getElementsByTagName(`body`); // find body element
+
+// append a child button to the last child of body
+
+// give the button an id that we can target
+
+// set the button as a variable
+
+// add an event listener to the button that will fetch when clicked
+
+// END TESTING
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
