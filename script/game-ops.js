9d226113c672bd376758dab8e95ab67925b0bada
stevendreed <steven.daniel.reed@gmail.com>
2023-09-21 20:27:30 -0700

    moved test functions to app.js

--- script/game-ops.js
+++ script/game-ops.js
@@ -72,39 +72,3 @@ const getGameByName = function(gameToFind)
     return rawgFetch(qp, RAWG_URL, RAWG_KEY);
 } // end getGameByName
 
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// TESTING
-// comb document for element to append a button to, for testing
-
-const testBtnEl = document.getElementById(`testing-form`);
-
-testBtnEl.addEventListener(`submit`, function(event)
-{
-    console.log(`submitted!`);
-    event.preventDefault(); // stop page from automatically refreshing
-    
-    // get data entered in input element
-    // let inputData = document.getElementById(`input-field`).formdata
-    let inputData = document.getElementById(`data`).value;
-
-    // console.log(inputData);
-    // pass data as fetch query
-    // rawgFetch(concatQuerPar(inputData), RAWG_URL);
-    //console.log(rawgFetch(inputData, RAWG_URL, RAWG_KEY));
-    console.log(getGameByName(inputData));
-} // end function
-); // end testBtnEl
-
-// find body element
-const bodyEl = document.getElementsByTagName(`body`); // find body element
-
-// append a child button to the last child of body
-
-// give the button an id that we can target
-
-// set the button as a variable
-
-// add an event listener to the button that will fetch when clicked
-
-// END TESTING
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
