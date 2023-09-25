eb3c91d9d2e1c35a8e14f76063ad6c6d9035443c
stevendreed <steven.daniel.reed@gmail.com>
2023-09-25 16:25:04 -0700

    added initialize function for LS

--- script/storage-ops.js
+++ script/storage-ops.js
@@ -6,45 +6,83 @@
 
 */
 
+/*
+   initLS = a function that initializes a local storage array of objects,
+   if the expected key is blank
+*/
+
+const initLS = function(key = `gameArray`)
+{
+    let outcome = 0;
+    if (!Window.localStorage[`gameArray`])
+    {
+        let tempArOfObj = [
+        ];
+        tempArOfObj = JSON.stringify(tempArOfObj);
+        localStorage.setItem(`gameArray`, tempArOfObj);
+        outcome = 1; // success: default initialized
+    } // end if
+    else
+    {
+        console.log(`an error has occurred with local storage`);
+    } // end else
+    return outcome;
+} // end initLS
+
+/*
+   pullFromLS = a function that grabs stringified local storage array
+   and returns it as an array of JSON objects
+*/
+const pullFromLS = function(key = `gameArray`)
+{
+
+} // end pullFromLS
+
+/*
+   pushToLS = a function that accepts a JSON, updates the array of objects,
+   then stores this stringified array in local storage
+*/
+
 // setCount = set the current number of stored games to value
 // stores the input under the count key 
-const setCount = function(value)
-{
-    value = toString(value);
-    localStorage.setItem(`count`, `${value}`);
-}
+// const setCount = function(value)
+// {
+//     value = toString(value);
+//     localStorage.setItem(`count`, `${value}`);
+// }
 
-// retrieveCount = retrieve the current count of stored games
-// retrieves the value under the count key, and returns as a parsed
-// float value
-const retrieveCount = function()
-{
-    return parseFloat(localStorage.getItem(`count`));
-}
+// // retrieveCount = retrieve the current count of stored games
+// // retrieves the value under the count key, and returns as a parsed
+// // float value
+// const retrieveCount = function()
+// {
+//     return parseFloat(localStorage.getItem(`count`));
+// }
 
-// bookmarkGame => save to local storage
-// first, retrieves the current game count, then uses that number to
-// set a key for a new stored item
-// finally, increments the stored count by 1 and stores that new value
-const bookmarkGame = function(game)
-{
-    // exception handling if count has not been defined yet
-    if (!Window.localStorage[`count`])
-    {
-        setCount(0);
-    } // end if
+// // bookmarkGame => save to local storage
+// // first, retrieves the current game count, then uses that number to
+// // set a key for a new stored item
+// // finally, increments the stored count by 1 and stores that new value
+// const bookmarkGame = function(game)
+// {
+//     // exception handling if count has not been defined yet
+//     if (!Window.localStorage[`count`])
+//     {
+//         setCount(0);
+//     } // end if
 
-    // retrieve current game count
-    let c = retrieveCount + 1;
+//     // retrieve current game count
+//     let c = retrieveCount + 1;
 
-    // create a key for the stored item
-    let tempKey = `game${c}`;
+//     // create a key for the stored item
+//     let tempKey = `game${c}`;
 
-    // save with key
-    localStorage.setItem(tempKey, game);
+//     // save with key
+//     localStorage.setItem(tempKey, game);
 
-    // update game count
-    setCount(c);
+//     // update game count
+//     setCount(c);
 
-    return;
-} // end bookmarkGame
+//     return;
+// } // end bookmarkGame
+
